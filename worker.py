b3684b709085f5a3fabdb028c74c2d7640601437
sdevilly <98481475+sdevilly@users.noreply.github.com>
2022-12-19 21:14:49 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -38,7 +38,7 @@ def addWorker(token, num):
       tdata=json.load(p)
     tdata['name']='slave'+str(num)
     data=json.dumps(tdata)
-    url='https://www.googleapis.com/compute/v1/projects/spark-371009/zones/europe-west1-b/instances'
+    url='https://www.googleapis.com/compute/v1/projects/optimum-airfoil-370211/zones/europe-west1-b/instances'
     headers={"Authorization": "Bearer "+token}
     resp=requests.post(url,headers=headers, data=data)
     if resp.status_code==200:     
